cc3061e0fbb39ce3e8dbe9939b16d69c03b6e4ab
fandi7418 <mgesitalifandi@gmail.com>
2020-06-20 08:36:19 +0700

    tugas UAS RSBK

---
+++ src/programrelawan/Programrelawan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programrelawan;
+
+/**
+ *
+ * @author User ID
+ */
+public class Programrelawan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         stock prog = new stock();
+        prog.setVisible(true);
+    }
+    
+}
